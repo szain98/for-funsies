4b9b244fa1d3ace9819904175442d9bb3d55ed58
Sana Zain <35517461+szain98@users.noreply.github.com>
2019-01-03 13:30:39 -0800

    added references and some file writing coding

--- comma.ai/JSONReader.java
+++ comma.ai/JSONReader.java
@@ -3,23 +3,27 @@ import org.json.simple*
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 
 public class JSONReader {
     //I need the speed for all coordinates for each trip
-    //one trip is one JSON file
-    //this parser will just deal with one file at a time
-    //a separate method or class will deal with parsing the entire directory
+    //one trip = one JSON file
     //the JSON file contains an ARRAY of coordinates and speed sampled at once per second
+    //Idea: parse through original JSON files and transfer data into a NEW geoJSON file
     
     //code based on:
     //https://www.mkyong.com/java/json-simple-example-read-and-write-json/
+    //JSON file writing references: 
+    //https://leafletjs.com/examples/geojson/
+    //https://stackoverflow.com/questions/31728446/write-a-json-file-in-java
     
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
         
         try {
+            FileWriter geoFile = new FileWriter();
             Object o = parser.parse(new FileReader("data path to file folder"));
             JSONObject jo = (JSONObject) o;
             //JSONObjects = (name, value) pairs, noted by curly brackets {}
